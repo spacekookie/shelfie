20ed09275b71bc87102e18ec44a3e4f3e55596e5
Milan Pässler <me@pbb.lc>
2019-05-03 20:33:43 +0200

    fix nixfile

--- default.nix
+++ default.nix
@@ -4,18 +4,13 @@ rustPlatform.buildRustPackage rec {
   name = "shelfie-${version}";
   version = "0.1.0";
 
-  src = fetchFromGitLab {
-    owner = "spacekookie";
-    repo = "shelfie";
-    rev = version;
-    sha256 = "0000000000000000000000000000000000000000000000000000";
-  };
+  src = ./.;
 
-  cargoSha256 = "0000000000000000000000000000000000000000000000000000";
+  cargoSha256 = "06nhwxyv2x78gdky5lz9raipx6z764n9ffqwq30x4mfxbsbdwf2x";
 
   meta = with lib; {
     description = "A small space to upload pictures to";
-    license = licenses.agpl;
+    license = licenses.agpl3;
     maintainers = [ maintainers.spacekookie ];
     platforms = platforms.all;
   };
