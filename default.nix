fb09d6d9ff94ea0679cf6406a3153735d49f38cd
Milan Pässler <me@pbb.lc>
2020-04-18 13:24:13 +0200

    use niv and naersk for Nix packaging
    [naersk](https://github.com/nmattia/naersk) provides a Nix expression to
    build Cargo crates without using a fixed-output derivation. This brings
    many advantages compared to the rustPlatform expression from nixpkgs:
    - It is not necessary to keep the cargoSha256 up-to-date
    - Metadata is read from Cargo.toml and does not have to be maintained
      twice
    - All dependencies are built in a seperate derivation and do not have to
      be rebuilt with every change in shelfie
    
    I chose to use niv to have a fixed reference to naersk and nixpkgs,
    so that users do not have to worry about incompatible changes in newer
    versions if they do not want to.

--- default.nix
+++ default.nix
@@ -1,17 +1,15 @@
-{ stdenv, lib, fetchFromGitLab, rustPlatform }:
+{ sources ? import ./nix/sources.nix
+, pkgs ? import sources.nixpkgs {}
+, shelfieSrc ? fetchGit ./. }:
 
-rustPlatform.buildRustPackage rec {
-  name = "shelfie-${version}";
-  version = "0.1.0";
-
-  src = ./.;
-
-  cargoSha256 = "06nhwxyv2x78gdky5lz9raipx6z764n9ffqwq30x4mfxbsbdwf2x";
-
-  meta = with lib; {
-    description = "A small space to upload pictures to";
-    license = licenses.agpl3;
-    maintainers = [ maintainers.spacekookie ];
-    platforms = platforms.all;
-  };
-}
+let
+  naersk = pkgs.callPackage sources.naersk {};
+in
+  (naersk.buildPackage shelfieSrc) // {
+    meta = with pkgs.lib; {
+      description = "A small space to upload pictures to";
+      license = licenses.agpl3;
+      maintainers = [ maintainers.spacekookie ];
+      platforms = platforms.all;
+    };
+  }
